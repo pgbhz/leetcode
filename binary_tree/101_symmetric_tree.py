dac0189348771644cad0886e87c0dfc8d48d3643
Pedro <pedrogabrielbhz@gmail.com>
2025-05-19 07:25:36 -0300

    Fix method name typo in symmetric tree check and improve clarity of comments

--- binary_tree/101_symmetric_tree.py
+++ binary_tree/101_symmetric_tree.py
@@ -10,14 +10,17 @@ from typing import Optional
 
 
 class Solution:
-    def isMirroed(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:  # type: ignore
+    def isMirror(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # the three next ifs basically check if they're the same
         if p is None and q is None:
             return True
         if p is None or q is None:
             return False
         if p.val != q.val:
             return False
-        return self.isMirroed(p.left, q.right) and self.isMirroed(p.right, q.left)  # type: ignore
 
-    def isMirroed(self, root: Optional[TreeNode]) -> bool:
-        return self.isMirroed(root.left, root.right)  # type: ignore
+        # the mirroing logic goes here. we compare the outer subtrees and the inner subtrees.
+        return self.isMirror(p.left, q.right) and self.isMirror(p.right, q.left)
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.isMirror(root.left, root.right)
