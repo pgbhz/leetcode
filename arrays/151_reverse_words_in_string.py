c9ff1a20eeec73a15ec82a68c33d2db98a89f158
Pedro Gabriel Amorim Soares <pedrogabrielbhz@gmail.com>
2025-05-22 15:02:06 -0300

    I hope there is life after death.

--- arrays/151_reverse_words_in_string.py
+++ arrays/151_reverse_words_in_string.py
@@ -15,3 +15,34 @@ class Solution:
             if word != "":
                 result += word + " "
         return result[:-1]
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        """
+        Input: s = "the sky is blue"
+                    012345678901234
+                    012
+        Output: "blue is sky the"
+        """
+        stack = []
+        word_start_idx = 0
+        started_seeing_word = False
+        for i in range(len(s)):
+            if s[i] != " " and not started_seeing_word:
+                started_seeing_word = True
+                word_start_idx = i
+
+            elif s[i] == " " and started_seeing_word:
+                word = s[word_start_idx:i]
+                stack.append(word)
+                started_seeing_word = False
+
+        if started_seeing_word:
+            word = s[word_start_idx : len(s)]
+            stack.append(word)
+
+        reversed_words = ""
+        while stack:
+            reversed_words += stack.pop() + " "
+        return reversed_words[:-1]
