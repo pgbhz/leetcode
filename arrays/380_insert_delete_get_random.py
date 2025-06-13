50bb953ea39e1d5cc9a7d47d83e54d716b29a772
Pedro Gabriel <pedrogabrielbhz@gmail.com>
2025-06-13 18:51:29 -0300

    Shabbath

---
+++ arrays/380_insert_delete_get_random.py
@@ -0,0 +1,52 @@
+"""
+which data structure gives us random access to it's members ? idk
+this smells like a linked list ... my nose keeps tricking me
+
+ok so we basically can generate a random number
+the trick is then to have two data structures coexisting:
+a hashmap
+and a list of keys
+
+but how do we have quick access to the key to be deleted ? i mean, how we quickly update the list after key removal ?
+we swap the key with the last element of the list, but how do we find the key quickly ? i guess this would take linear traversal ...
+
+we need to keep track of the keys...
+"""
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.values = {}
+        self.keys = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.values:
+            return False
+        self.values[val] = len(self.keys)
+        self.keys.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.values:
+            return False
+        index = self.values[val]
+        last_val = self.keys[-1]
+
+        self.keys[index], self.keys[-1] = self.keys[-1], self.keys[index]
+        self.values[last_val] = index
+        self.keys.pop()
+
+        del self.values[val]
+        return True
+
+    def getRandom(self) -> int:
+        rand_idx = randint(0, len(self.keys) - 1)
+        return self.keys[rand_idx]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
--- prefix_sum/525_subarray_sums_k.py
+++ prefix_sum/525_subarray_sums_k.py
@@ -1,11 +1,53 @@
-# class Solution:
-#     def subarraySum(self, nums: List[int], k: int) -> int:
-#         totalSubarraysWithSumK = 0
-#         for i in range(len(nums)):
-#             subarraySum = 0
-#             for j in range(i, len(nums)):
-#                 subarraySum += nums[j]
-#                 if subarraySum == k:
-#                     totalSubarraysWithSumK += 1
-#         return totalSubarraysWithSumK
+"""
+1. Repeat the question.
 
+    Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0s and 1s.
+
+2. Ask clarifying questions.
+3. Work through examples.
+
+    nums = [0,1] -> answer = 2
+
+    nums = [0, 1, 0] -> answer = 2
+
+    nums = [0,1,1,1,1,1,0,0,0] -> answer = 6
+
+4. Brainstorm solutions.
+
+    - What is the brute force?
+
+        Well, we can just test every damn subarray.
+
+        We can use, perhaps, a two pointers approach.
+            How? We keep a count. Each time we see a zero, we decrement. Each time we see a one, we increment.
+            Each time we change from a positive value to a negative value, we're testing a new array, basically.
+
+5. Implement your solution.
+
+    - I'm doing it, bro! Cool. Done.
+
+6. Test your solution.
+"""
+
+from typing import List
+
+
+class Solution:
+    # [0,1,1,1,1,1,0,0,0]
+    def findMaxLength(self, nums: List[int]) -> int:
+        max_length = 0
+        parity_count = 0
+        positions = {0: -1}
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                parity_count -= 1
+            else:
+                parity_count += 1
+
+            if parity_count in positions:
+                max_length = max(max_length, (i - positions[parity_count]))
+            else:
+                positions[parity_count] = i
+
+        return max_length
